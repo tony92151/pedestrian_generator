f29b6b1ff461e9961a82cee3cb5e2e403bbf4235
tony <tony92151@gmail.com>
2020-04-12 13:42:44 +0800

    add self-atten

--- models.py
+++ models.py
@@ -3,6 +3,57 @@ import torch.nn as nn
 import torch.nn.functional as F
 from layers import Flatten, Concatenate
 
+from torch.nn.utils import spectral_norm
+
+# https://github.com/heykeetae/Self-Attention-GAN/blob/master/sagan_models.py
+def snconv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True):
+    return spectral_norm(nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+                                   stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias))
+class Self_Attn(nn.Module):
+    """ Self attention Layer"""
+
+    def __init__(self, in_channels):
+        super(Self_Attn, self).__init__()
+        self.in_channels = in_channels
+        self.snconv1x1_theta = snconv2d(in_channels=in_channels, out_channels=in_channels//8, kernel_size=1, stride=1, padding=0)
+        self.snconv1x1_phi = snconv2d(in_channels=in_channels, out_channels=in_channels//8, kernel_size=1, stride=1, padding=0)
+        self.snconv1x1_g = snconv2d(in_channels=in_channels, out_channels=in_channels//2, kernel_size=1, stride=1, padding=0)
+        self.snconv1x1_attn = snconv2d(in_channels=in_channels//2, out_channels=in_channels, kernel_size=1, stride=1, padding=0)
+        self.maxpool = nn.MaxPool2d(2, stride=2, padding=0)
+        self.softmax  = nn.Softmax(dim=-1)
+        self.sigma = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x):
+        """
+            inputs :
+                x : input feature maps(B X C X W X H)
+            returns :
+                out : self attention value + input feature 
+                attention: B X N X N (N is Width*Height)
+        """
+        _, ch, h, w = x.size()
+        # Theta path
+        theta = self.snconv1x1_theta(x)
+        theta = theta.view(-1, ch//8, h*w)
+        # Phi path
+        phi = self.snconv1x1_phi(x)
+        phi = self.maxpool(phi)
+        phi = phi.view(-1, ch//8, h*w//4)
+        # Attn map
+        attn = torch.bmm(theta.permute(0, 2, 1), phi)
+        attn = self.softmax(attn)
+        # g path
+        g = self.snconv1x1_g(x)
+        g = self.maxpool(g)
+        g = g.view(-1, ch//2, h*w//4)
+        # Attn_g
+        attn_g = torch.bmm(g, attn.permute(0, 2, 1))
+        attn_g = attn_g.view(-1, ch//2, h, w)
+        attn_g = self.snconv1x1_attn(attn_g)
+        # Out
+        out = x + self.sigma*attn_g
+        return out
+
 
 class CompletionNetwork(nn.Module):
     def __init__(self):
@@ -31,6 +82,10 @@ class CompletionNetwork(nn.Module):
         self.conv6 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
         self.bn6 = nn.BatchNorm2d(256)
         self.act6 = nn.ReLU()
+        
+        # self
+        self.self_attn1 = Self_Attn(256)
+        
         # input_shape: (None, 256, img_h//4, img_w//4)
         self.conv7 = nn.Conv2d(256, 256, kernel_size=3, stride=1, dilation=2, padding=2)
         self.bn7 = nn.BatchNorm2d(256)
@@ -39,6 +94,10 @@ class CompletionNetwork(nn.Module):
         self.conv8 = nn.Conv2d(256, 256, kernel_size=3, stride=1, dilation=4, padding=4)
         self.bn8 = nn.BatchNorm2d(256)
         self.act8 = nn.ReLU()
+        
+        # self
+        self.self_attn2 = Self_Attn(256)
+        
         # input_shape: (None, 256, img_h//4, img_w//4)
         self.conv9 = nn.Conv2d(256, 256, kernel_size=3, stride=1, dilation=8, padding=8)
         self.bn9 = nn.BatchNorm2d(256)
@@ -83,8 +142,10 @@ class CompletionNetwork(nn.Module):
         x = self.bn4(self.act4(self.conv4(x)))
         x = self.bn5(self.act5(self.conv5(x)))
         x = self.bn6(self.act6(self.conv6(x)))
+        x = self.self_attn1(x)
         x = self.bn7(self.act7(self.conv7(x)))
         x = self.bn8(self.act8(self.conv8(x)))
+        x = self.self_attn2(x)
         x = self.bn9(self.act9(self.conv9(x)))
         x = self.bn10(self.act10(self.conv10(x)))
         x = self.bn11(self.act11(self.conv11(x)))
@@ -291,10 +352,18 @@ class GlobalDiscriminator_P(nn.Module):
         self.conv2 = nn.Conv2d(64, 128, kernel_size=5, stride=2, padding=2)
         self.bn2 = nn.BatchNorm2d(128)
         self.act2 = nn.ReLU()
+        
+        # self
+        self.self_attn1 = Self_Attn(128)
+        
         # input_shape: (None, 128, img_h//4, img_w//4)
         self.conv3 = nn.Conv2d(128, 256, kernel_size=5, stride=2, padding=2)
         self.bn3 = nn.BatchNorm2d(256)
         self.act3 = nn.ReLU()
+        
+        # self
+        self.self_attn2 = Self_Attn(256)
+        
         # input_shape: (None, 256, img_h//8, img_w//8)
         self.conv4 = nn.Conv2d(256, 512, kernel_size=5, stride=2, padding=2)
         self.bn4 = nn.BatchNorm2d(512)
@@ -317,7 +386,9 @@ class GlobalDiscriminator_P(nn.Module):
     def forward(self, x):
         x = self.bn1(self.act1(self.conv1(x)))
         x = self.bn2(self.act2(self.conv2(x)))
+        x = self.self_attn1(x)
         x = self.bn3(self.act3(self.conv3(x)))
+        x = self.self_attn2(x)
         x = self.bn4(self.act4(self.conv4(x)))
         x = self.bn5(self.act5(self.conv5(x)))
         

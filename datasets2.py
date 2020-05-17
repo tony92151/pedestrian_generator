beecf5fb226792d54283b860b0f31f6a7d923b57
tony <tony92151@gmail.com>
2020-05-17 16:55:55 +0800

    add mask loss

--- datasets2.py
+++ datasets2.py
@@ -22,12 +22,16 @@ class ImageDataset(data.Dataset):
         self.street_imgpaths = sorted(glob.glob(data_dir+'/street/*'), key=lambda x: x[-10:-3])
         self.people_imgpaths = sorted(glob.glob(data_dir+'/people/*'), key=lambda x: x[-10:-3])
         self.position_jsonpaths = sorted(glob.glob(data_dir+'/json/*'), key=lambda x: x[-10:-3])
-        self.poeple_masks = sorted(glob.glob(data_dir+'/mask/*'), key=lambda x: x[-10:-3])
+        self.poeple_maskspaths = sorted(glob.glob(data_dir+'/mask/*'), key=lambda x: x[-10:-3])
         
         self.load2meme = load2meme
         if (self.load2meme):
             #print("Load all images to mem")
-            self.pre_tre_imgs, self.pre_peo_imgs, self.left_top= self.img_in_mem(self.street_imgpaths, self.people_imgpaths, self.position_jsonpaths, self.poeple_masks)
+            self.pre_tre_imgs, self.pre_peo_imgs, self.pre_mask, self.pre_left_top= self.img_in_mem(
+                self.street_imgpaths, 
+                self.people_imgpaths, 
+                self.position_jsonpaths, 
+                self.poeple_maskspaths )
             
 
     def __len__(self):
@@ -35,22 +39,23 @@ class ImageDataset(data.Dataset):
 
     def __getitem__(self, index):
         if (self.load2meme):
-            out1, out2, out3 = self.pre_tre_imgs[index], self.pre_peo_imgs[index], np.array(self.left_top[index])
+            # out1: street img
+            # out2: people with mask img
+            # out3: mask img
+            # out4: poeple position
+            out1, out2, out3, out4 = self.pre_tre_imgs[index], self.pre_peo_imgs[index], self.pre_mask[index], np.array(self.pre_left_top[index])
         else:
             # Load street image
-            img = Image.open(self.street_imgpaths[index])
+            street_img = Image.open(self.street_imgpaths[index])
             people_img = Image.open(self.people_imgpaths[index])
-            masks_imgs = Image.open(self.poeple_masks[index])
-            #people_img = Image.fromarray(mask_img_composite(self.people_imgpaths[index],(64,128))[:,:,[2,1,0]])
-            #masks_imgs = Image.fromarray((cv2.cvtColor(np.asarray(masks_imgs).astype(np.uint8) , cv2.COLOR_GRAY2RGB)*np.asarray(people_img))[:,:,[2,1,0]])
-            
-            people_mask = Image.open(self.poeple_masks[index])
+            masks_img = Image.open(self.poeple_maskspaths[index])
             
             #img = img.convert('RGB')
 
             # Create plain mask image
-            plain_mask = Image.fromarray(np.zeros((img.size[1],img.size[0]),dtype="uint8"))
+            plain_mask = Image.fromarray(np.zeros((street_img.size[1],street_img.size[0]),dtype="uint8"))
             plain_mask = plain_mask.convert('RGB')
+            
 
             # Load data information from .json file
             with open(self.position_jsonpaths[index]) as json_file:
@@ -58,70 +63,45 @@ class ImageDataset(data.Dataset):
 
             box_x, box_y, box_w, box_h = int(data[0]['pos'][0]), int(data[0]['pos'][1]), int(data[0]['pos'][2]),int(data[0]['pos'][3])
 
-            cw, ch = math.floor(img.size[0]/2), math.floor(img.size[1]/2)
+            cw, ch = math.floor(street_img.size[0]/2), math.floor(street_img.size[1]/2)
 
-            plain_mask.paste(people_img,(box_x,box_y),masks_imgs)
+            plain_mask.paste(people_img,(box_x,box_y),masks_img)
             
             lt = [box_x-cw+128,box_y-ch+128]
 
-            input_img = img.crop((cw-128, ch-128, cw+128, ch+128)) #left, top, right, bottom
+            input_img = street_img.crop((cw-128, ch-128, cw+128, ch+128)) #left, top, right, bottom
             mask_with_poeple = plain_mask.crop((cw-128, ch-128, cw+128, ch+128)) #left, top, right, bottom       
+
+            plain_mask.paste(masks_img,(box_x,box_y))
+            plain_mask = plain_mask.crop((cw-128, ch-128, cw+128, ch+128))
 
             #input_img = input_img.convert('RGB')
             if self.transform is not None:
                 input_img = self.transform(input_img)
                 mask_with_poeple = self.transform2(mask_with_poeple)
-            out1, out2, out3 = input_img, mask_with_poeple, np.array(lt)
+                plain_mask = self.transform2(plain_mask)
+            out1, out2, out3, out4 = input_img, mask_with_poeple, plain_mask ,np.array(lt)
             
-        return out1, out2, out3
+        return out1, out2, out3, out4
 
 
     def img_in_mem(self, str_img, people_img, position_json, mask_poeple):
         str_imgs = []
         mask_people_imgs = []
-        left_top = []
+        pre_left_top = []
+        pre_mask = []
         pbar = tqdm(total=len(str_img))
         
         for index in range(len(str_img)):
-#             img = Image.open(str_img[index])
-#             #peo_img = Image.open(people_img[index])
-#             peo_img = Image.fromarray(mask_img_composite(self.people_imgpaths[index],(64,128))[:,:,[2,1,0]])
-#             img = img.convert('RGB')
-
-#             # Create plain mask image
-#             plain_mask = Image.fromarray(np.zeros((img.size[1],img.size[0]),dtype="uint8"))
-#             plain_mask = mask.convert('RGB')
-
-#             # Load data information from .json file
-#             with open(position_json[index]) as json_file:
-#                 data = json.load(json_file)
-
-#             box_x, box_y, box_w, box_h = int(data[0]['pos'][0]), int(data[0]['pos'][1]), int(data[0]['pos'][2]),int(data[0]['pos'][3])
-
-#             cw, ch = math.floor(img.size[0]/2), math.floor(img.size[1]/2)
-
-#             palin_mask.paste(people_img,(box_x,box_y),masks_imgs)
-
-#             input_img = img.crop((cw-128, ch-128, cw+128, ch+128)) #left, top, right, bottom
-#             mask_with_poeple = mask.crop((cw-128, ch-128, cw+128, ch+128)) #left, top, right, bottom    
-            
-#             lt = [96,64]
-            
-#             input_img = input_img.convert('RGB')
-#             mask_with_poeple = mask_with_poeple.convert('RGB')
             # Load street image
-            img = Image.open(self.street_imgpaths[index])
+            street_img = Image.open(self.street_imgpaths[index])
             people_img = Image.open(self.people_imgpaths[index])
-            masks_imgs = Image.open(self.poeple_masks[index])
-            #people_img = Image.fromarray(mask_img_composite(self.people_imgpaths[index],(64,128))[:,:,[2,1,0]])
-            #masks_imgs = Image.fromarray((cv2.cvtColor(np.asarray(masks_imgs).astype(np.uint8) , cv2.COLOR_GRAY2RGB)*np.asarray(people_img))[:,:,[2,1,0]])
-            
-            people_mask = Image.open(self.poeple_masks[index])
+            masks_img = Image.open(self.poeple_maskspaths[index])
             
             #img = img.convert('RGB')
 
             # Create plain mask image
-            plain_mask = Image.fromarray(np.zeros((img.size[1],img.size[0]),dtype="uint8"))
+            plain_mask = Image.fromarray(np.zeros((street_img.size[1],street_img.size[0]),dtype="uint8"))
             plain_mask = plain_mask.convert('RGB')
 
             # Load data information from .json file
@@ -130,24 +110,27 @@ class ImageDataset(data.Dataset):
 
             box_x, box_y, box_w, box_h = int(data[0]['pos'][0]), int(data[0]['pos'][1]), int(data[0]['pos'][2]),int(data[0]['pos'][3])
 
-            cw, ch = math.floor(img.size[0]/2), math.floor(img.size[1]/2)
+            cw, ch = math.floor(street_img.size[0]/2), math.floor(street_img.size[1]/2)
 
-            plain_mask.paste(people_img,(box_x,box_y),masks_imgs)
+            plain_mask.paste(people_img,(box_x,box_y),masks_img)
             
             lt = [box_x-cw+128,box_y-ch+128]
 
-            input_img = img.crop((cw-128, ch-128, cw+128, ch+128)) #left, top, right, bottom
-            mask_with_poeple = plain_mask.crop((cw-128, ch-128, cw+128, ch+128)) #left, top, right, bottom       
+            input_img = street_img.crop((cw-128, ch-128, cw+128, ch+128)) #left, top, right, bottom
+            mask_with_poeple = plain_mask.crop((cw-128, ch-128, cw+128, ch+128)) #left, top, right, bottom      
 
-            #input_img = input_img.convert('RGB')
+            plain_mask.paste(masks_img,(box_x,box_y))
+            plain_mask = plain_mask.crop((cw-128, ch-128, cw+128, ch+128))        
             
             if self.transform is not None:
                 input_img = self.transform(input_img)
                 mask_with_poeple = self.transform2(mask_with_poeple)
+                plain_mask = self.transform2(plain_mask)
                 
             str_imgs.append(input_img)
             mask_people_imgs.append(mask_with_poeple)
-            left_top.append(lt)
+            pre_left_top.append(lt)
+            pre_mask.append(plain_mask)
             pbar.update()
         pbar.close()
-        return str_imgs, mask_people_imgs, left_top
+        return str_imgs, mask_people_imgs, pre_mask, pre_left_top
--- losses.py
+++ losses.py
@@ -4,7 +4,7 @@ import torch
 def completion_network_loss(input, output, mask):
     return mse_loss(output * mask, input * mask)
 
-def completion_network_loss_P(background, person, output, lefttop, height = 64 , width = 32, devices = 'cuda:0'):
+def completion_network_loss_P(background, person, mask, output, lefttop, height = 64 , width = 32, devices = 'cuda:0'):
     """
     * background:
         - shape: batchsize * 3 * 256 * 256
@@ -13,6 +13,7 @@ def completion_network_loss_P(background, person, output, lefttop, height = 64 ,
     """
     output_crop = torch.zeros([output.shape[0],3,128,64]).to(devices)
     person_crop = torch.zeros([output.shape[0],3,128,64]).to(devices)
+    mask_crop = torch.zeros([output.shape[0],3,128,64]).to(devices)
     
     #mask = torch.zeros(size = output.shape).to(devices)
     batchsize = output.shape[0]
@@ -20,7 +21,8 @@ def completion_network_loss_P(background, person, output, lefttop, height = 64 ,
         left , top = lefttop[i][0],lefttop[i][1]
         output_crop[i,:,:,:] = output[i,:,top : top + height , left : left + width]
         person_crop[i,:,:,:] = person[i,:,top : top + height , left : left + width]
+        mask_crop[i,:,:,:] = mask[i,:,top : top + height , left : left + width]
         #mask[ i ,:, top : top + height , left : left + width]  = 1
 
-    return 0.7*mse_loss(output_crop, person_crop) + 0.3*mse_loss(output, background)
+    return 0.5*mse_loss(output_crop, person_crop) + 0.35*mse_loss(output, background) + 0.15*(-1)*mse_loss(mask_crop,person_crop)
 

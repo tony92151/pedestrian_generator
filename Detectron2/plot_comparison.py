0cd0ecf3ab056ec8f7587dc958bd7df9455e5354
antony_aiforge <a8252525@gmail.com>
2020-08-12 22:37:21 +0800

    add plot comparison

---
+++ Detectron2/plot_comparison.py
@@ -0,0 +1,51 @@
+import pylab
+import matplotlib.pyplot as plt
+import csv
+import os
+######################################################################################
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-a', '--all_csv_path', type=str, default=None)
+
+parser.add_argument('-o','--output_dir', type=str, default=None)
+
+parser.add_argument('-f')
+args = parser.parse_args()
+
+######################################################################################
+
+csv_paths = args.all_csv_path
+
+output_dir = args.output_dir
+
+output_path = output_dir+'/comparison.jpg'
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+leg = list()
+
+for csv_path in os.listdir(csv_paths):
+    x = []
+    y = []
+    p = csv_paths + csv_path
+    with open(p, newline='') as csvfile:
+        rows = csv.reader(csvfile)
+        for row in rows:
+            x.append(float(row[0]))
+            y.append(float(row[1]))
+    line, = ax.plot(x ,y , 'o-')
+    leg.append(csv_path[:-4])
+    
+
+ax.legend( leg, loc='lower left', shadow=True)
+
+    
+
+ax.set_xscale('log')
+ax.set_yscale('log')
+
+plt.savefig(output_path)
+
+print("Image save at :", output_path)
+# plt.show()
